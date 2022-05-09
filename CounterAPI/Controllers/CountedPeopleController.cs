a4dea18f23893b919984eb195322eded29c6a38d
Rika1700 <78212506+Rika1700@users.noreply.github.com>
2022-05-09 20:34:12 +0200

    added range function

--- CounterAPI/Controllers/CountedPeopleController.cs
+++ CounterAPI/Controllers/CountedPeopleController.cs
@@ -2,6 +2,7 @@
 using CounterAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System;
 
 namespace CountedAPI.Controllers
 {
@@ -31,6 +32,48 @@ namespace CountedAPI.Controllers
 			}
 
 			return countedDay;
+		}
+
+		[HttpGet("{date1}/{date2}")]
+		public ActionResult<DateRange> GetRange(string date1, string date2)
+		{
+			List<PeopleCount> days = new List<PeopleCount>();
+			List<string> rangeString = new List<string>();
+			List<double> rangeDouble = new List<double>();
+			int count;
+			double avg;
+			DateTime dateTime1;
+			DateTime dateTime2;
+
+			try
+			{
+				dateTime1 = DateTime.Parse(date1.Substring(0, 10));
+				dateTime2 = DateTime.Parse(date2.Substring(0, 10));
+			}
+			catch (FormatException)
+			{
+				return NotFound();
+			}
+			for (; dateTime1 <= dateTime2; dateTime1 = dateTime1.AddDays(1)) {
+				days.Add(GetDetailsObject(dateTime1.ToString("yyyy-MM-dd")));
+				rangeString.Add(dateTime1.ToString("yyyy-MM-dd"));
+			}
+			foreach (var day in days) {
+				count = 0;
+				avg = 0.0;
+				if (day.Arr == null) {
+					rangeDouble.Add(avg);
+					continue;
+				}
+				for (int i = 1; i < 11; i++) {
+					count += day.Arr[i];
+				}
+				avg = (count/10.0);
+				rangeDouble.Add(avg);
+			}
+
+
+			return new DateRange(rangeString.ToArray(), rangeDouble.ToArray());
 		}
 
 		[HttpGet("{date}/peoplecount")]
@@ -148,6 +191,54 @@ namespace CountedAPI.Controllers
 			_countedService.Remove(idDate);
 
 			return NoContent();
+		}
+
+		public PeopleCount GetDetailsObject(string date)
+		{
+
+			int peopleCounted = 0;
+			int maxPeopleCounted = 0;
+			int time = 6;
+			int[] array = new int[15];
+			int arrCount = 0;
+			var countedDay = _countedService.Get(date);
+
+			if (countedDay == null)
+			{
+				return null;
+			}
+
+			for (var index = 0; index < countedDay.Count;)
+			{
+				if (time > 20)
+				{
+					break;
+				}
+				while (countedDay[index].date_and_time.Contains(" " + (time < 10 ? "0" + time.ToString() : time.ToString()) + ":"))
+				{
+					if (countedDay[index].personIn == true)
+					{
+						peopleCounted++;
+					}
+					if (countedDay[index].personIn == false && peopleCounted > 0)
+					{
+						peopleCounted--;
+					}
+					if (index == countedDay.Count - 1)
+					{
+						break;
+					}
+					index++;
+				}
+				array[arrCount] = peopleCounted;
+				arrCount++;
+				time += 1;
+			}
+			if (countedDay.Count == 0)
+			{
+				array = null;
+			}
+			return new PeopleCount(peopleCounted, maxPeopleCounted, array);
 		}
 	}
 }
---
+++ CounterAPI/Models/DateRange.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CounterAPI.Models
+{
+    public class DateRange
+    {
+        public string[] Dates { get; set; }
+
+        public double[] Avg { get; set; }
+
+        public DateRange(string[] str, double[] avg) {
+            Dates = str;
+            Avg = avg;
+        }
+    }
+}
